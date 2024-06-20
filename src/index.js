c5beb5d4f1174b280dc320bca3212b75cb82298d
pedrofaleiros <pedrocrfaleiros@yahoo.com.br>
2024-06-20 11:13:46 -0300

    barra de progresso

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import fs from "fs";
 import { listPeriodicos } from "./repository/PeriodicoRepository.js";
 import { listPeriodicoAreas } from "./repository/AreaPeriodicoRepository.js";
+import cliProgress from "cli-progress";
 
 const fileName = "dados.csv";
 
@@ -8,6 +9,15 @@ async function main() {
   const data = await listPeriodicos();
 
   var content = "ID;Titulo;ISSN;Area;Linguagem;Revisado por pares\n";
+
+  const progressBar = new cliProgress.SingleBar(
+    {
+      format: "{bar} | {percentage}% | {value}/{total} periodicos",
+    },
+    cliProgress.Presets.shades_classic
+  );
+
+  progressBar.start(data.length, 0);
 
   for (var i = 0; i < data.length; i++) {
     const el = data[i];
@@ -20,7 +30,10 @@ async function main() {
       content += `${el.id};${titulo};${el.issn};${area.areaNome};${linguagem};${revisado};\n`;
     });
     // content += `${el.id};"${titulo}";${el.issn};"AREA";"${linguagem}";${revisado};\n`;
+    progressBar.update(i);
   }
+
+  progressBar.stop();
 
   fs.writeFile(fileName, content, function (err) {
     if (err) throw err;
