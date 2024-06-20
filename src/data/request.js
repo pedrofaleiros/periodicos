760e1016f2897a289b7b7cc65e5dd8fea48190cb
pedrofaleiros <pedrocrfaleiros@yahoo.com.br>
2024-06-20 12:29:09 -0300

    cookie

--- src/data/request.js
+++ src/data/request.js
@@ -13,29 +13,5 @@ function getCookie() {
 }
 
 export const headers = {
-  // Accept:
-  //   "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-  // "Accept-Encoding": "gzip, deflate, br, zstd",
-  // "Accept-Language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
-  // "Cache-Control": "max-age=0",
-  // Connection: "keep-alive",
   Cookie: getCookie(),
-  // Host: "www-periodicos-capes-gov-br.ez106.periodicos.capes.gov.br",
-  // Referer: "https://www-periodicos-capes-gov-br.ez106.periodicos.capes.gov.br/",
-  // "Sec-Fetch-Dest": "document",
-  // "Sec-Fetch-Mode": "navigate",
-  // "Sec-Fetch-Site": "same-origin",
-  // "Sec-Fetch-User": "?1",
-  // "Upgrade-Insecure-Requests": "1",
-  // "User-Agent":
-  //   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
-  // "sec-ch-ua":
-  //   '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
-  // "sec-ch-ua-mobile": "?0",
-  // "sec-ch-ua-platform": '"Windows"',
 };
-
-// module.exports = {
-//   BASE_URL,
-//   headers,
-// };
